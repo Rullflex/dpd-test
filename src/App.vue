89ed0f966476124f6b6e1bae61242bac6791cbfd
Dmitry Gorshkov <libafer99@gmail.com>
2022-11-24 17:06:58 +0500

    feat: add table data

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,94 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+    <div id="app">
+        <table>
+            <thead>
+                <tr>
+                    <th>Avatar</th>
+                    <th>Full name</th>
+                    <th>Gender</th>
+                    <th>Country</th>
+                    <th>Birth date</th>
+                    <th>Email</th>
+                    <th>Phone number</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr v-for="(row, rowIdx) in tableData" :key="rowIdx">
+                    <td><img :src="row.picture.medium" alt="" /></td>
+                    <td>{{ row.name.first }} {{ row.name.last }}</td>
+                    <td>{{ row.gender }}</td>
+                    <td>{{ row.location.country }}</td>
+                    <td>{{ row.dob.date }}</td>
+                    <td>{{ row.email }}</td>
+                    <td>{{ row.phone }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
+import { Component, Vue } from 'vue-property-decorator';
+
+interface Location {
+    city: string;
+    coordinates: { latitude: string; longitude: string };
+    country: string;
+    postcode: number;
+    state: string;
+}
+
+interface User {
+    cell: string;
+    dob: {
+        age: number;
+        date: string;
+    };
+    email: string;
+    gender: 'male' | 'female';
+    id: {
+        name: string;
+        value: string;
+    };
+    location: Location;
+    name: {
+        first: string;
+        last: string;
+        title: string;
+    };
+    nat: string;
+    phone: string;
+    picture: {
+        large: string;
+        medium: string;
+        thumbnail: string;
+    };
+    registered: {
+        age: number;
+        date: string;
+    };
+}
 
 @Component({
-  components: {
-    HelloWorld,
-  },
+    components: {},
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+    tableData: User[] = [];
+
+    async created() {
+        const res: { results: User[] } = await (await fetch('/api.json')).json();
+        this.tableData = res.results;
+    }
+}
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
